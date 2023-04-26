d7313f8ca0fb096f118406ce33e10aa1f55ac270
Naoko Sugawara <infoseek122@gmail.com>
2023-04-26 15:53:35 -0400

    added some functions on borads.js

--- src/board.js
+++ src/board.js
@@ -9,6 +9,21 @@ if (typeof window === 'undefined'){
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
+  const grid = [];
+
+  for (let i=0; i < 8; i++){
+    grid[i] = [];
+    for (let j=0; j < 8; j++){
+      grid[i][j] = [];
+    }
+  }
+
+  grid[3, 3] = new Piece("white");
+  grid[3, 4] = new Piece("black");
+  grid[4, 3] = new Piece("black");
+  grid[4, 4] = new Piece("white");
+
+  console.log(grid);
 }
 
 /**
